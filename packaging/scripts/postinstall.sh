b2e5d17f0eef2772d68293045ba433af45816831
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2020-07-28 10:19:13 +0900

    add goreleaser setting

---
+++ packaging/scripts/postinstall.sh
@@ -0,0 +1,3 @@
+systemctl daemon-reload
+systemctl enable slo-adapter.service
+systemctl restart slo-adapter.service
---
+++ packaging/scripts/preremove.sh
@@ -0,0 +1,2 @@
+systemctl stop slo-adapter.service
+systemctl disable slo-adapter.service
